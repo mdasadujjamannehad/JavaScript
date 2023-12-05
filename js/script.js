d95198b22b9816615e535c5b609154aba108c463
Asadujjaman Nehad <146873165+asadujjamannehad@users.noreply.github.com>
2023-12-06 00:09:41 +0600

    Java Script Mark Sheet

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let marks = 1
+
+if(marks<=100 && marks>=80){
+    console.log('You Got A+')
+}
+else if(marks<=79 && marks>=70) {
+        console.log('You Got A')
+}
+else if(marks<=69 && marks>=60) {
+        console.log('You Got A-')
+}
+else if(marks<=59 && marks>=50) {
+        console.log('You Got B')
+}
+else if(marks<=49 && marks>=40) {
+        console.log('You Got C')
+}
+else if(marks<=39 && marks>=33) {
+        console.log('You Got D')
+}
+else if(marks<=32 && marks>=0) {
+        console.log('You are Failed')
+}
+else{
+    console.log('Beta Al-Amin Sir er cheye beshi bujho?')
+}
